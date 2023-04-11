738a46875a0fb64cd78a8a495643d6b513bc0857
bohdanh <bohdan.stephen@gmail.com>
2023-04-10 22:54:58 -0400

    Adding to syllable count and final score variable

--- analyze.py
+++ analyze.py
@@ -16,21 +16,29 @@ def count_syllables_in_word(word):
     last_char = word[-1]
 
     if last_char in endings:
+        processed_word = word[0:-1]
+    else:
         processed_word = word
 
-    if len(word) <= 3:
+    if len(processed_word) <= 3:
         return 1
+    
+    if processed_word[-1] in 'eE':
+        processed_word = processed_word[0:-1]
 
     vowels = "aeiouAEIOU"
     prev_char_was_vowel = False
 
-    for char in word:
+    for char in processed_word:
         if char in vowels:
            if not prev_char_was_vowel:
             count = count + 1
            prev_char_was_vowel = True
         else:
-            prev_char_was_vowel = False   
+            prev_char_was_vowel = False
+
+    if processed_word[-1] in 'yY':
+        count = count + 1           
 
     return count
 
@@ -54,9 +62,13 @@ def compute_readability(text):
     total_words = len(words)
     total_sentences = count_sentences(text)
     total_syllables = count_syllables(words)
+
+    score = (206.835 - 1.015 * (total_words / total_sentences)
+             -84.06 * (total_syllables / total_words))
     
     print(total_words, 'words')
     print(total_sentences, 'sentences')
     print(total_syllables, 'syllables')
+    print(score, 'reading ease score')
 
 compute_readability(ch1text.text)
