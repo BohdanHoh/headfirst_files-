ea920dafb66819655a075f85e2cb7d5e1f22220f
bohdanh <bohdan.stephen@gmail.com>
2023-06-25 19:05:39 -0400

    finishing up crazy lib program

--- chp_9/crazy.py
+++ chp_9/crazy.py
@@ -1,9 +1,39 @@
-for i in range(0, 1000):
-    filename = str(i) + '.txt'
+def make_crazy_lib(filename):
     file = open(filename, 'r')
-    text = file.read()
-    if 'needle' in text:
-        print('Found needle in file ' + str(i) + '.txt')
-        break
+
+    text = ''
+
+    for line in file:
+        text = text + process_line(line) + '\n'
+
     file.close()
-print('Scan complete')
+
+    return text
+
+placeholders = ['NOUN', 'ADJECTIVE', 'VERB_ING', 'VERB']
+
+def process_line(line):
+    global placeholders
+    processed_line = ''
+
+    words = line.split()
+
+    for word in words:
+        stripped = word.strip('.,;?!')
+        if stripped in placeholders:
+            answer = input('Enter a ' + stripped + ':')
+            processed_line = processed_line + answer
+            if word[-1] in '.,;?!':
+                processed_line = processed_line + word[-1] + ' ' 
+            else:
+                processed_line = processed_line + ' '
+        else:
+            processed_line = processed_line + word + ' '
+    return processed_line + '\n'
+
+def main():
+    lib = make_crazy_lib('lib.txt')
+    print(lib)
+
+if __name__ == '__main__':
+    main()
